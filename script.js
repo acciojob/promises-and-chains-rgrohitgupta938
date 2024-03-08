7289e0af9465da44d977822d0768d66006968980
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-08 16:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById("ageForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  if (!name || !age) {
+    alert("Please fill in all fields.");
+  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  });
+  promise
+    .then((name) => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch((err) => {
+      alert(`Oh sorry, ${name}. You aren't old enough.`);
+    });
+});
